0efc3420ed514311a35dc84c7f8595fb4b20b327
mikeneirinck <mikeneirinck@Mikes-Mac-mini.local>
2014-12-03 21:12:49 +0100

    added colors to solution

--- MNTableViewController.m
+++ MNTableViewController.m
@@ -70,10 +70,13 @@
     // Configure the cell...
     if (indexPath.section == 0) {
         cell.textLabel.text = @"I am in section 0";
+        cell.backgroundColor = [UIColor redColor];
     } else if(indexPath.section == 1){
         cell.textLabel.text = @"another section";
+        cell.backgroundColor = [UIColor blueColor];
     } else {
         cell.textLabel.text = [NSString stringWithFormat:@"cell %i", indexPath.row];
+        cell.backgroundColor = [UIColor yellowColor];
     }
     
     return cell;
